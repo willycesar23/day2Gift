d7732f3a4de9f0705587952ee3ac68edbd629992
DELL <DELL@10.0.0.179>
2023-04-07 20:35:20 -0400

    3rd change

--- src/day2/day2Demo.java
+++ src/day2/day2Demo.java
@@ -7,6 +7,7 @@ public class day2Demo {
 		public void getInfo() {
 			System.out.println("Pushing code...");
 			System.out.println("Pushing 2code...");
+			System.out.println("pushing 3 code");
 		}
 		
 		
